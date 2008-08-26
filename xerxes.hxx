e79f72cafd3ddb2f179fe433feb45f5a9a25ad7d
Jan Losinshi <losinshi@atlantis.wh2.tu-dresden.de>
2008-08-26 17:48:43 +0200

    debug support added

---
+++ xerxes.hxx
@@ -0,0 +1,15 @@
+/**
+ * xerxes - mysql proxying
+ * ``Why do you persist in your loneliness?'' --Xerxes
+ * (c) 2008 
+ * Jan Losinski <losinshi@wh2.tu-dresden.de> 
+ * Maximilian Marx <mmarx@wh2.tu-dresden.de>
+ */	
+
+#ifndef XERXES_XERXES_HXX
+#define XERXES_XERXES_HXX
+
+extern int be_quiet;
+extern int be_debug;
+
+#endif
